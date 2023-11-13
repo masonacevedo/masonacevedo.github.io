512ef8ba7157c408b7f8c23193c6a50d9c8c5e6a
Mason Acevedo <macevedo@hmc.edu>
2023-11-13 09:05:32 -0800

    2 changes
    backend bug fixes, change frontend to tell user about bird prediction rather than image metadata.

--- my_app.py
+++ my_app.py
@@ -6,42 +6,57 @@ import PIL
 import io
 import pathlib
 
+
 from fastai.vision.all import *
-from fastai.callback.core import Callback
 
-class MyVerboseCallback(Callback):
-    def after_batch(self):
-        if self.iter % 100 == 0:
-            print(f"Batch: {self.iter}, Training Loss: {self.loss}")
-
+app = flask.Flask(__name__)
 
 posix_backup = pathlib.PosixPath
 try:
     pathlib.PosixPath = pathlib.WindowsPath
     model_path = pathlib.Path("C:/Users/undoc/Desktop/Fun_Stuff/Code/Github_Stuff/masonacevedo.github.io/fast_ai_model_with_normalization_with_architecture.pkl")
-    learn_inf = load_learner(model_path)
+                            
+    model = load_learner(model_path)
 finally:
     pathlib.PosixPath = posix_backup
 
-app = flask.Flask(__name__)
+
 flask_cors.CORS(app, origins=["https://masonacevedo.github.io"])
 
 @app.route('/check-for-bird', methods = ["POST"])
 def check_for_bird():
+
     image = flask.request.files.get("user_image")
     if image is None:
         return flask.jsonify(error = "No image provided"),400
+
+    print("Image:")
+    print(image)
+
+    PIL_image_object = Image.open(io.BytesIO(image.read()))
+    PIL_image_object.save("C:/Users/undoc/Downloads/pil_image.jpg")
+    fastai_image = PILImage.create(PIL_image_object)
+    fastai_image.save("C:/Users/undoc/Downloads/fast_image.jpg")
+
+    prediction, probability = categorize_image(fastai_image)
+    return flask.jsonify(prediction = (prediction), probability = (probability))
+
+
+def categorize_image(im):
+    print("type(im)",type(im))
+    resized_image = Resize(192, method = 'squish')(im)
+
+    mean = [0.4771, 0.4596, 0.4162]
+    stddev = [0.2288, 0.2205, 0.2198]
+
+    normalized_resized_image = Normalize.from_stats(mean, stddev)(resized_image)
+
+    prediction = model.predict(normalized_resized_image)
+
+    category = str(prediction[0])
+    if category == "bird":
+        probability = prediction[2][2].item()
+    else:
+        probability = 1 - prediction[2][2].item()
     
-    # uploaded image -> PIL image object.
-    PIL_image_obj = PIL.Image.open(io.BytesIO(image.read()))
-
-    # Convert PIL image to PyTorch Tensor
-    transform_obj = torchvision.transforms.ToTensor()
-    tensor_image = transform_obj(PIL_image_obj)
-
-    # get metadata
-    num_channels, height, width = tensor_image.shape
-
-    
-
-    return flask.jsonify(width = (width), height = (height))
+    return category, probability
